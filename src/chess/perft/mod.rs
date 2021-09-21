ab608bb8f59d4be95b4a2dd3f208d1969a8c0e5d
tsatke <tim.satke@gmail.com>
2021-09-21 17:36:43 +0200

    add a perft implementation

---
+++ src/chess/perft/mod.rs
@@ -0,0 +1,37 @@
+use crate::chess::board::piece::Color;
+use crate::chess::board::Board;
+
+pub fn perft(depth: usize, board: &mut Board, color: Color) -> usize {
+    if depth == 0 {
+        return 1;
+    }
+    board
+        .generate_moves(color)
+        .into_iter()
+        .map(|m| board.make_move(m))
+        .filter_map(|r| r.ok())
+        .map(|mut b| perft(depth - 1, &mut b, color.other()))
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::chess::board::setup::default_setup;
+
+    #[test]
+    fn test_perft_table() {
+        for table in [(0, 1), (1, 20), (2, 400), (3, 8902)] {
+            let mut b = Board::new();
+            b.populate(default_setup);
+
+            assert_eq!(
+                table.1,
+                perft(table.0, &mut b, Color::White),
+                "assert perft({}) == {}",
+                table.0,
+                table.1
+            );
+        }
+    }
+}
